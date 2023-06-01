46969b3e5c10242c43cb19c9900428a096373f9f
Mrinal Fouzdar <100467560+mrinalFouzdar@users.noreply.github.com>
2023-06-01 20:31:45 +0530

    add drawer-navigator features

---
+++ screens/WelcomeScreen.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const WelcomeScreen = () => {
+  return (
+    <View style={styles.rootContainer}>
+      <Text>
+        This is the <Text style={styles.higlight}>"Welcome"</Text> screen!
+      </Text>
+    </View>
+  )
+}
+
+export default WelcomeScreen
+
+
+const styles = StyleSheet.create({
+    rootContainer:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    highlight: {
+      fontWeight: 'bold',
+      color: '#eb1064',
+    },
+})
